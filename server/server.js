b35b00c10b02b13b0424473be51005496fa86708
muhurtkumar <muhurtkumar@gmail.com>
2025-08-05 02:44:02 +0530

    Integrated MongoDB connection

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import connectDB from './config/db.js';
 
 // Initialize the Express application
 const app = express();
+
+// Database connection
+await connectDB()
 
 // Middlewares
 app.use(cors());
