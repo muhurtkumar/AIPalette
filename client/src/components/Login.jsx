5597e0f9d8c22fd496926c2b995bc80d9284bd43
muhurtkumar <muhurtkumar@gmail.com>
2025-08-05 01:46:46 +0530

    Enhanced Login modal with multi-step signup and profile photo upload

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,9 +1,14 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { FaUser } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import { RiLockPasswordLine } from "react-icons/ri";
+import upload_area from '../assets/upload_area.svg';
+import { IoMdClose } from "react-icons/io";
+import { AppContext } from '../context/AppContext';
 
 const Login = () => {
+
+    const {setShowLogin} = useContext(AppContext);
 
     const[state, setState] = useState('Login')
     const [name, setName] = useState('')
@@ -11,14 +16,38 @@ const Login = () => {
     const [email, setEmail] = useState('')
 
     const [image, setImage] = useState(false);
-    const [isNextDataSubmitted, setIsNextDataSubmitted] = useState(false);
+    const [isTextDataSubmitted, setIsTextDataSubmitted] = useState(false);
+
+    const onSubmitHandler = async(e) => {
+        e.preventDefault();
+        if(state == "Sign Up" && !isTextDataSubmitted){
+            setIsTextDataSubmitted(true);
+        }
+    }
+
+    useEffect(()=> {
+        document.body.style.overflow = 'hidden'
+        return () => {
+            document.body.style.overflow = 'unset'
+        }
+    }, [])
 
     return (
         <div className='absolute top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
-            <form className='relative bg-white p-10 rounded-xl text-slate-500'>
+            <form onSubmit={onSubmitHandler} className='relative bg-white p-10 rounded-xl text-slate-500'>
                 <h1 className='text-center text-2xl text-neutral-700 font-medium'>{state} Form</h1>
                 <p className='text-sm'>Welcome back! Please sign in to continue</p>
-                <>
+                {state === "Sign Up" && isTextDataSubmitted
+                ? <>
+                    <div className='flex items-center gap-4 my-10'>
+                        <label htmlFor="image">
+                            <img className='w-16 rounded-full cursor-pointer' src={image ? URL.createObjectURL(image) : upload_area} alt="" />
+                            <input onChange={e => setImage(e.target.files[0])} type="file" id='image' hidden />
+                        </label>
+                        <p>Upload Profile <br /> Picture</p>
+                    </div>
+                </>
+                : <>
                     {state !== 'Login' && (
                         <div className='border px-4 py-2 flex items-center gap-2 rounded-full mt-5'>
                             <FaUser />
@@ -33,17 +62,17 @@ const Login = () => {
                         <RiLockPasswordLine />
                         <input className='outline-none text-sm' onChange={e => setPassword(e.target.value)} type="password" placeholder='Enter password' required value={password} />
                     </div>
-
-                    <p className='text-sm text-blue-600 my-4 cursor-pointer'>Forgot password?</p>
-                </>
-                <button className='bg-blue-600 w-full text-white py-2 rounded-full'>
-                    {state === 'Login' ? 'Login' : 'Create Account'}
+                </>}
+                {state === "Login" && <p className='text-sm text-blue-600 mt-4 cursor-pointer'>Forgot password?</p>}
+                <button type='submit' className='bg-blue-600 w-full text-white py-2 rounded-full mt-4'>
+                    {state === 'Login' ? 'Login' : isTextDataSubmitted ? 'Create Account' : 'Next'}
                 </button>
                 {
                     state === 'Login'
                     ? <p className='mt-5 text-center '>Don't have an account? <span className='text-blue-600 cursor-pointer' onClick={()=> setState("Sign Up")}>Sign Up</span></p>
                     : <p className='mt-5 text-center '>Already have an account? <span className='text-blue-600 cursor-pointer' onClick={()=> setState("Login")}>Login</span></p>
                 }
+                <IoMdClose onClick={() => setShowLogin(false)} className="absolute top-5 right-5 text-xl text-gray-600 hover:text-red-500 cursor-pointer"/>
             </form>
         </div>
     )
