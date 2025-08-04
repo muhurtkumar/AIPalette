6af3bb60fb34e84f2fe36f2170039ea54c896b35
muhurtkumar <muhurtkumar@gmail.com>
2025-08-05 02:56:30 +0530

    Created User model

---
+++ server/models/User.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({ 
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+});
+
+const User = mongoose.model('User', userSchema); 
+
+export default User;
